fa0160ebfec54e86b6f2a83bdfbfb51cf8efa01f
Amir3an <amiralisayad0019@gmail.com>
2024-11-22 15:53:21 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <header>
+    <headerCnt />
+  </header>
+</template>
+
+<script>
+import headerCnt from "./components/headerCnt.vue";
+
+export default {
+  components: {
+    headerCnt,
+  },
+};
+</script>
+
+<style>
+.app {
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 12, 1);
+  margin: auto;
+  font-family: Barlow;
+  color: white;
+}
+#app {
+  width: 90%;
+}
+</style>
