0eb250e7a0edc9f1b9839dd5d2da3d7d76d76cbf
Amir3an <amiralisayad0019@gmail.com>
2024-11-25 16:25:17 +0330

    12

--- src/components/headerCnt.vue
+++ src/components/headerCnt.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="pic" class="m-auto flex">
+  <img
+    class="fixed z-[-99] object-cover w-full h-full"
+    src="@/assets/img/1.png"
+  />
+  <div id="pic" class="w-[100%] m-auto h-[550px] flex">
     <ul class="w-[100%] flex h-10">
       <p class="text-white text-xl">EyesOn</p>
       <a class="text-xl text-[#FFFFFF]">Home</a>
@@ -13,13 +17,6 @@
 <script></script>
 
 <style scoped>
-#pic {
-  width: 100%;
-  height: 1100px;
-  background: url("../assets/img/1.png") center no-repeat;
-  background-size: center;
-  object-fit: cover;
-}
 div {
   font-family: BanglaSanga;
 }
