a5cbcf37ffcf6b346ad190ae84806108adc30d85
Amir3an <amiralisayad0019@gmail.com>
2024-11-25 17:31:52 +0330

    37

--- src/components/navCnt.vue
+++ src/components/navCnt.vue
@@ -1,9 +1,13 @@
 <template>
-  <p class="text-white">im in</p>
+  <h2 class="text-white text-5xl">What is our mission</h2>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoepd>
+h2 {
+  font-family: BarlowRegula;
+}
+</style>
