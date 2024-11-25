b778f3293f0095f8ae402e5426b8590f4609a97f
Amir3an <amiralisayad0019@gmail.com>
2024-11-25 16:34:19 +0330

    14

--- src/components/headerCnt.vue
+++ src/components/headerCnt.vue
@@ -3,7 +3,7 @@
     class="relative z-[-99] object-cover w-full h-full"
     src="../assets/img/1.png"
   />
-  <div id="pic" class="w-[100%] m-auto h-[550px] flex">
+  <div class="flex absolute top-12 w-[90%] items-center m-auto h-10 bg-red-300">
     <ul class="w-[100%] flex h-10">
       <p class="text-white text-xl">EyesOn</p>
       <a class="text-xl text-[#FFFFFF]">Home</a>
