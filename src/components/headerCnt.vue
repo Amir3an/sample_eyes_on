64b5a981f58e89d9e78bc7827dca0db5aaa0a592
Amir3an <amiralisayad0019@gmail.com>
2024-11-25 16:55:55 +0330

    25

--- src/components/headerCnt.vue
+++ src/components/headerCnt.vue
@@ -7,10 +7,20 @@
     class="w-[80%] absolute left-[10%] top-12 flex items-center justify-between h-10"
   >
     <p class="text-white text-xl">EyesOn</p>
-    <ul class="flex w-[395px] justify-between">
-      <li><a class="text-xl font-normal cursor-pointer text-[#FFFFFF]">Home</a></li>
-      <li><a class="text-xl font-normal cursor-pointer text-[#FFFFFF]">Services</a></li>
-      <li><a class="text-xl font-normal cursor-pointer text-[#FFFFFF]">Portfolio</a></li>
+    <ul class="flex w-[395px] justify-between bg-red-200">
+      <li>
+        <a class="text-xl font-normal cursor-pointer text-[#FFFFFF]">Home</a>
+      </li>
+      <li>
+        <a class="text-xl font-normal cursor-pointer text-[#FFFFFF]"
+          >Services</a
+        >
+      </li>
+      <li>
+        <a class="text-xl font-normal cursor-pointer text-[#FFFFFF]"
+          >Portfolio</a
+        >
+      </li>
     </ul>
 
     <p class="text-[#F1EDBA]">Motion Graphics</p>
