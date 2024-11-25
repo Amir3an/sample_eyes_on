7e724808a9f063cde631ff7cec416a4f8c1c5d25
Amir3an <amiralisayad0019@gmail.com>
2024-11-25 16:40:25 +0330

    16

--- src/components/headerCnt.vue
+++ src/components/headerCnt.vue
@@ -3,21 +3,24 @@
     class="relative z-[-99] object-cover w-full h-full"
     src="../assets/img/1.png"
   />
-  <ul
-    class="flex translate-y-0 top-12 w-[90%] items-center m-auto h-10 bg-red-300"
+  <div
+    class="w-[90%] absolute left-[5%] top-12 flex items-center justify-between h-10 bg-red-300"
   >
     <p class="text-white text-xl">EyesOn</p>
-    <a class="text-xl text-[#FFFFFF]">Home</a>
-    <a class="text-xl text-[#FFFFFF]">Services</a>
-    <a class="text-xl text-[#FFFFFF]">Portfolio</a>
+    <ul class="w-20 flex">
+      <li><a class="text-xl text-[#FFFFFF]">Home</a></li>
+      <li><a class="text-xl text-[#FFFFFF]">Services</a></li>
+      <li><a class="text-xl text-[#FFFFFF]">Portfolio</a></li>
+    </ul>
+
     <p class="text-[#F1EDBA]">Motion Graphics</p>
-  </ul>
+  </div>
 </template>
 
 <script></script>
 
 <style scoped>
-ul {
+div {
   font-family: BanglaSanga;
 }
 </style>
