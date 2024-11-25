9e97b1aa35ae05d72fe9f1f2149df355e1927b33
Amir3an <amiralisayad0019@gmail.com>
2024-11-25 17:18:16 +0330

    29

--- src/components/headerCnt.vue
+++ src/components/headerCnt.vue
@@ -27,8 +27,45 @@
   </div>
 </template>
 
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      isVisible: false, // مدیریت نمایش متن
+      observer: null,
+    };
+  },
+  mounted() {
+    console.log("Component mounted, setting up observer");
 
+    // ایجاد یک Observer برای مشاهده‌ی تغییرات در عنصر
+    this.observer = new IntersectionObserver(this.handleIntersect, {
+      threshold: 0.1, // وقتی 10 درصد از عنصر دیده شود، تریگر می‌شود
+    });
+
+    this.$nextTick(() => {
+      const el = this.$refs.observeItem; // گرفتن رفرنس عنصر
+      if (el) {
+        this.observer.observe(el); // شروع مشاهده‌ی عنصر
+      }
+    });
+  },
+  methods: {
+    handleIntersect(entries) {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          this.isVisible = true; // نمایش عنصر وقتی که در viewport قرار می‌گیرد
+        }
+      });
+    },
+  },
+  beforeUnmount() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  },
+};
+</script>
 <style scoped>
 div {
   font-family: BanglaSanga;
