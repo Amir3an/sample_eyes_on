845f3895b1a0a3de138a8f44ebe5adb3a94d138c
Amir3an <amiralisayad0019@gmail.com>
2024-11-26 19:03:32 +0330

    42

--- src/components/fstContent.vue
+++ src/components/fstContent.vue
@@ -17,8 +17,7 @@
     <div class="w-[594px] h-auto">
       <div id="img" class="rounded-3xl">
         <div
-          id="blur"
-          class="w-full relative h-[180px] px-10 py-8 rounded-3xl bg-[#70F9E01A]"
+          class="w-full relative h-[180px] px-10 py-8 rounded-3xl bg-[#70F9E01A] backdrop-blur-[21px]"
         >
           <h2 class="text-2xl font-bold">Motion Graphic</h2>
           <p class="text-xl mt-4">
@@ -27,12 +26,16 @@
           </p>
         </div>
       </div>
-      <div class="w-full h-[180px] px-10 py-8 mt-5 rounded-3xl bg-[#70F9E01A]">
-        <h2 class="text-2xl font-bold">2D & 3D Animation</h2>
-        <p class="text-xl mt-4">
-          Detailed infortmation about our motion graphics and<br />
-          animation services, including examples and benefits.
-        </p>
+      <div id="img" class="rounded-3xl">
+        <div
+          class="w-full h-[180px] px-10 py-8 mt-5 rounded-3xl bg-[#70F9E01A] backdrop-blur-[21px] brightness-75"
+        >
+          <h2 class="text-2xl font-bold">2D & 3D Animation</h2>
+          <p class="text-xl mt-4">
+            Detailed infortmation about our motion graphics and<br />
+            animation services, including examples and benefits.
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -57,7 +60,6 @@ button {
 }
 
 #img {
-  backdrop-filter: brightness(600%);
   background: url("../assets/img/eyseOn1.png");
 }
 </style>
