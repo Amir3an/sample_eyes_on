dced783900da6b8ca746ac6079c3c7db58a376f9
Amir3an <amiralisayad0019@gmail.com>
2024-11-27 00:29:24 +0330

    11

--- src/components/secContent.vue
+++ src/components/secContent.vue
@@ -10,23 +10,25 @@
     </div>
 
     <div class="w-full flex mt-12 h-[620px] rounded-3xl bg-red-200">
-      <div
-        class="w-[50%] flex flex-col gap-8 justify-center h-full rounded-3xl bg-red-200 pl-[5%]"
-      >
-        <span class="text-[#F1EDBA]">3D Design</span>
-        <h2 class="text-3xl font-bold">
-          A brand 3d Design<br />
-          Characteristic for growth
-        </h2>
-        <p class="text-xl">
-          Detailed infortmation about our motion graphics and<br />
-          animation services, including examples and benefits.
-        </p>
-        <button
-          class="w-44 h-14 text-[#F1EDBA] text-xl bg-[#002C3F] rounded-[32px]"
+      <div id="img" class="rounded-3xl">
+        <div
+          class="w-[50%] bg-[#70F9E01A] backdrop-blur-[21px] relative flex flex-col gap-8 justify-center h-full rounded-3xl bg-red-200 pl-[5%]"
         >
-          View Project
-        </button>
+          <span class="text-[#F1EDBA]">3D Design</span>
+          <h2 class="text-3xl font-bold">
+            A brand 3d Design<br />
+            Characteristic for growth
+          </h2>
+          <p class="text-xl">
+            Detailed infortmation about our motion graphics and<br />
+            animation services, including examples and benefits.
+          </p>
+          <button
+            class="w-44 h-14 text-[#F1EDBA] text-xl bg-[#002C3F] rounded-[32px]"
+          >
+            View Project
+          </button>
+        </div>
       </div>
       <img
         class="w-[50%] h-full rounded-3xl object-cover"
@@ -48,5 +50,9 @@ div {
 
 button {
   border: 1px solid #f1edba;
+}
+
+#img {
+  background: url("../assets/img/card.png");
 }
 </style>
