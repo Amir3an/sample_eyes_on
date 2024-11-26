40e09d8359f11b242dcc260c3abbbac21e489529
Amir3an <amiralisayad0019@gmail.com>
2024-11-26 16:43:28 +0330

    24

--- src/components/fstContent.vue
+++ src/components/fstContent.vue
@@ -5,6 +5,12 @@
       Detailed infortmation about our motion graphics and<br />
       animation services, including examples and benefits.
     </p>
+
+    <button
+      class="w-44 h-14 text-[#F1EDBA] text-xl bg-[#002C3F] rounded-[32px]"
+    >
+      View More
+    </button>
   </div>
 </template>
 
