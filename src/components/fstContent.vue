05e638f4aa5e57664dfe633e22b5b8295979fa90
Amir3an <amiralisayad0019@gmail.com>
2024-11-26 16:50:57 +0330

    29

--- src/components/fstContent.vue
+++ src/components/fstContent.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="w-[80%] flex m-auto">
-    <h2 class="text-5xl">Services</h2>
-    <p class="text-xl">
-      Detailed infortmation about our motion graphics and<br />
-      animation services, including examples and benefits.
-    </p>
+    <div class="flex flex-col gap-8">
+      <h2 class="text-5xl font-bold">Services</h2>
+      <p class="text-xl">
+        Detailed infortmation about our motion graphics and<br />
+        animation services, including examples and benefits.
+      </p>
 
-    <button
-      class="w-44 h-14 text-[#F1EDBA] text-xl bg-[#002C3F] rounded-[32px]"
-    >
-      View More
-    </button>
+      <button
+        class="w-44 h-14 text-[#F1EDBA] text-xl bg-[#002C3F] rounded-[32px]"
+      >
+        View More
+      </button>
+    </div>
   </div>
 </template>
 
