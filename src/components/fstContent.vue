b3809a15e93faeb1c3313faf469be69239a5e055
Amir3an <amiralisayad0019@gmail.com>
2024-11-26 18:03:17 +0330

    37

--- src/components/fstContent.vue
+++ src/components/fstContent.vue
@@ -15,15 +15,17 @@
     </div>
 
     <div class="w-[594px] h-auto">
-      <div
-        id="blur"
-        class="w-full h-[180px] px-10 py-8 rounded-3xl bg-[#70F9E01A]"
-      >
-        <h2 class="text-2xl font-bold">Motion Graphic</h2>
-        <p class="text-xl mt-4">
-          Detailed infortmation about our motion graphics and<br />
-          animation services, including examples and benefits.
-        </p>
+      <div id="img">
+        <div
+          id="blur"
+          class="w-full relative h-[180px] px-10 py-8 rounded-3xl bg-[#70F9E01A]"
+        >
+          <h2 class="text-2xl font-bold">Motion Graphic</h2>
+          <p class="text-xl mt-4">
+            Detailed infortmation about our motion graphics and<br />
+            animation services, including examples and benefits.
+          </p>
+        </div>
       </div>
       <div class="w-full h-[180px] px-10 py-8 mt-5 rounded-3xl bg-[#70F9E01A]">
         <h2 class="text-2xl font-bold">2D & 3D Animation</h2>
@@ -51,6 +53,10 @@ button {
 }
 
 #blur {
-  backdrop-filter: blur(20px);
+  backdrop-filter: blur(24px);
+}
+
+#img {
+  background: url("../assets/img/eyseOn1.png");
 }
 </style>
