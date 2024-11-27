6c3a635b5500ca989f5ee2aecc5c29a1b6a912f3
Amir3an <amiralisayad0019@gmail.com>
2024-11-27 17:21:52 +0330

    25

--- src/components/secContent.vue
+++ src/components/secContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-[80%] m-auto pb-20">
+  <div class="w-[80%] m-auto">
     <div class="w-full flex justify-between">
       <h2 class="text-5xl font-bold">Key Projects & Case Studies</h2>
       <button
@@ -9,31 +9,35 @@
       </button>
     </div>
 
-    <div class="w-full flex mt-12 h-[620px]">
-      <div id="img" class="w-[50%] h-full bg-[#70F9E01A] rounded-ss-3xl">
-        <div
-          class="w-full bg-[#70F9E01A] backdrop-blur-[21px] relative flex flex-col pb-14 gap-8 justify-end h-full rounded-s-3xl pl-[5%]"
-        >
-          <span class="text-[#F1EDBA]">3D Design</span>
-          <h2 class="text-3xl font-bold">
-            A brand 3d Design<br />
-            Characteristic for growth
-          </h2>
-          <p class="text-xl">
-            Detailed infortmation about our motion graphics and<br />
-            animation services, including examples and benefits.
-          </p>
-          <button
-            class="w-44 h-14 text-[#F1EDBA] text-xl bg-[#002C3F] rounded-[32px]"
+    <div id="img">
+      <div
+        class="w-full flex mt-12 h-[620px] rounded-3xl bg-[#70F9E01A] backdrop-blur-[21px]"
+      >
+        <div id="img" class="w-[50%] h-full bg-[#70F9E01A] rounded-ss-3xl">
+          <div
+            class="w-full bg-[#70F9E01A] backdrop-blur-[21px] relative flex flex-col gap-8 justify-end pb-14 h-full rounded-ss-3xl pl-[5%]"
           >
-            View Project
-          </button>
+            <span class="text-[#F1EDBA]">3D Design</span>
+            <h2 class="text-3xl font-bold">
+              A brand 3d Design<br />
+              Characteristic for growth
+            </h2>
+            <p class="text-xl">
+              Detailed infortmation about our motion graphics and<br />
+              animation services, including examples and benefits.
+            </p>
+            <button
+              class="w-44 h-14 text-[#F1EDBA] text-xl bg-[#002C3F] rounded-[32px]"
+            >
+              View Project
+            </button>
+          </div>
         </div>
+        <img
+          class="w-[50%] h-full rounded-3xl object-cover"
+          src="../assets/img/card.png"
+        />
       </div>
-      <img
-        class="w-[50%] h-full rounded-3xl object-cover"
-        src="../assets/img/card.png"
-      />
     </div>
   </div>
 </template>
