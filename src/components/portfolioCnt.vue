b523e0fb273fa3a33d896095ff74e0730ce6654b
Amir3an <amiralisayad0019@gmail.com>
2024-11-27 19:46:07 +0330

    39

--- src/components/portfolioCnt.vue
+++ src/components/portfolioCnt.vue
@@ -1,9 +1,44 @@
 <template>
-  <div class="w-[80%] flex m-auto"></div>
+  <swiper
+    :cssMode="true"
+    :navigation="true"
+    :pagination="true"
+    :mousewheel="true"
+    :keyboard="true"
+    :modules="modules"
+    class="mySwiper"
+  >
+    <swiper-slide>Slide 1</swiper-slide>
+    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
+    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
+    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
+    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
+  </swiper>
 </template>
-
 <script>
-export default {};
-</script>
+// Import Swiper Vue.js components
+import { Swiper, SwiperSlide } from "swiper/vue";
 
-<style></style>
+// Import Swiper styles
+import "swiper/css";
+
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "./style.css";
+
+// import required modules
+import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
+
+export default {
+  components: {
+    Swiper,
+    SwiperSlide,
+  },
+  setup() {
+    return {
+      modules: [Navigation, Pagination, Mousewheel, Keyboard],
+    };
+  },
+};
+</script>
