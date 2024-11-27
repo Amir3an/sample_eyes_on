6ada533c179cbe70e8b54ec7c70dc86fc75f140b
Amir3an <amiralisayad0019@gmail.com>
2024-11-27 18:32:20 +0330

    34

--- src/components/portfolioCnt.vue
+++ src/components/portfolioCnt.vue
@@ -1,15 +1,13 @@
 <template>
-  
+  <div class="w-[80%] flex m-auto">
+    
 
-  
+
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
