65a995126a318e53d2dd6bbedc330bb35fae1774
Amir3an <amiralisayad0019@gmail.com>
2024-11-28 16:50:14 +0330

    24

--- src/components/contactUs.vue
+++ src/components/contactUs.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="flex w-[80%] m-auto">
+  <div class="w-[80%] m-auto">
     <h2 class="text-white text-[56px] font-bold leading-tight w-[100%]">
       Contact us
     </h2>
+    <form class="w-[70%] h-[456px] bg-red-100"></form>
   </div>
 </template>
 
