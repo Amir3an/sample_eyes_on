858254ce8b377c435c48a32a7969d0876b28a561
Amir3an <amiralisayad0019@gmail.com>
2024-11-28 15:09:16 +0330

    13

--- src/assets/motion/victot1.vue
+++ src/assets/motion/victot1.vue
@@ -19,6 +19,7 @@
         gradientUnits="userSpaceOnUse"
         gradientTransform="translate(108.529 39.3144) scale(105.917 223.484)"
       >
+      
         <stop offset="0.03" stop-color="white" />
         <stop offset="0.13" stop-color="white" stop-opacity="0.81" />
         <stop offset="0.26" stop-color="white" stop-opacity="0.6" />
@@ -32,3 +33,7 @@
     </defs>
   </svg>
 </template>
+
+<style>
+
+</style>
