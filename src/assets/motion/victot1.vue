dff564ee6b35e5e7320da1b30c01ea666f9c53d2
Amir3an <amiralisayad0019@gmail.com>
2024-11-28 12:16:10 +0330

    09

---
+++ src/assets/motion/victot1.vue
@@ -0,0 +1,34 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="217"
+    height="273"
+    viewBox="0 0 217 273"
+    fill="none"
+  >
+    <path
+      d="M108.529 272.583C168.366 272.583 216.873 170.138 216.873 43.7649C216.873 -82.608 168.366 -185.053 108.529 -185.053C48.6932 -185.053 0.186295 -82.608 0.186295 43.7649C0.186295 170.138 48.6932 272.583 108.529 272.583Z"
+      fill="url(#paint0_radial_1_10882)"
+    />
+    <defs>
+      <radialGradient
+        id="paint0_radial_1_10882"
+        cx="0"
+        cy="0"
+        r="1"
+        gradientUnits="userSpaceOnUse"
+        gradientTransform="translate(108.529 39.3144) scale(105.917 223.484)"
+      >
+        <stop offset="0.03" stop-color="white" />
+        <stop offset="0.13" stop-color="white" stop-opacity="0.81" />
+        <stop offset="0.26" stop-color="white" stop-opacity="0.6" />
+        <stop offset="0.39" stop-color="white" stop-opacity="0.41" />
+        <stop offset="0.51" stop-color="white" stop-opacity="0.26" />
+        <stop offset="0.64" stop-color="white" stop-opacity="0.15" />
+        <stop offset="0.76" stop-color="white" stop-opacity="0.07" />
+        <stop offset="0.88" stop-color="white" stop-opacity="0.02" />
+        <stop offset="1" stop-color="white" stop-opacity="0" />
+      </radialGradient>
+    </defs>
+  </svg>
+</template>
