c79723bdc142bae2bdd8aaf22f14e1b5afaead41
Amir3an <amiralisayad0019@gmail.com>
2024-11-28 18:35:03 +0330

    28

--- src/components/contactUs.vue
+++ src/components/contactUs.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="w-[80%] m-auto">
     <h2 class="text-[56px] font-bold leading-tight w-[100%]">Contact us</h2>
-    <form class="w-[70%] p-8 mt-[5%] h-[456px] rounded-3xl bg-[#70F9E014]">
+    <form class="w-[60%] p-8 mt-[5%] h-[456px] rounded-3xl bg-[#70F9E014]">
       <div class="flex items-center justify-between">
-        <div class="w-[45%] flex flex-col">
+        <div class="w-[48%] flex flex-col">
           <label class="text-xl">First Name</label>
-          <input class="h-12 rounded-lg mt-3 outline-none" type="text" />
+          <input
+            class="bg-[#99DDE52E] text-xl p-[18px] h-12 rounded-lg mt-3 outline-none"
+            type="text"
+          />
         </div>
-        <div class="w-[45%] flex flex-col">
+        <div class="w-[48%] flex flex-col">
           <label class="text-xl">Last Name</label>
-          <input class="h-12 rounded-lg mt-3 outline-none" type="text" />
+          <input
+            class="bg-[#99DDE52E] text-xl p-[18px] h-12 rounded-lg mt-3 outline-none"
+            type="text"
+          />
         </div>
+      </div>
+      <div class="w-full mt- flex flex-col">
+        <lable class="text-xl">Email Address</lable>
+        <input
+          class="bg-[#99DDE52E] text-xl p-[18px] h-12 rounded-lg mt-3 outline-none"
+          type="text"
+        />
       </div>
     </form>
   </div>
