c08b4bcc6e9155c187fe0fb104187671b1a6064a
Amir3an <amiralisayad0019@gmail.com>
2024-11-28 18:27:54 +0330

    27

--- src/components/contactUs.vue
+++ src/components/contactUs.vue
@@ -1,9 +1,18 @@
 <template>
   <div class="w-[80%] m-auto">
-    <h2 class="text-white text-[56px] font-bold leading-tight w-[100%]">
-      Contact us
-    </h2>
-    <form class="w-[70%] mt-[5%] h-[456px] rounded-3xl bg-[#70F9E014]"></form>
+    <h2 class="text-[56px] font-bold leading-tight w-[100%]">Contact us</h2>
+    <form class="w-[70%] p-8 mt-[5%] h-[456px] rounded-3xl bg-[#70F9E014]">
+      <div class="flex items-center justify-between">
+        <div class="w-[45%] flex flex-col">
+          <label class="text-xl">First Name</label>
+          <input class="h-12 rounded-lg mt-3 outline-none" type="text" />
+        </div>
+        <div class="w-[45%] flex flex-col">
+          <label class="text-xl">Last Name</label>
+          <input class="h-12 rounded-lg mt-3 outline-none" type="text" />
+        </div>
+      </div>
+    </form>
   </div>
 </template>
 
@@ -14,5 +23,13 @@ export default {};
 <style scoped>
 h2 {
   font-family: Bangla-Sangam-MN;
+}
+
+div {
+  color: white;
+}
+
+input {
+  color: #ffffff;
 }
 </style>
