1037011dfa303bffb208e50bce801bf64679afbe
Amir3an <amiralisayad0019@gmail.com>
2024-11-30 21:28:30 +0330

    05

--- src/components/footerCnt.vue
+++ src/components/footerCnt.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="absolute top-0 right-0  w-[114.76px] h-[1079.27px] bg-red-100"></div>
+  <div
+    class="absolute top-0 right-0 w-[114.76px] h-[1079.27px] bg-red-100"
+  ></div>
 </template>
 
 <script></script>
