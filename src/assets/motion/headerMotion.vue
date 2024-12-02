9a881fe522cb4ef959db1cabb75751dcd55236b5
Amir3an <amiralisayad0019@gmail.com>
2024-12-02 15:28:17 +0330

    27

--- src/assets/motion/headerMotion.vue
+++ src/assets/motion/headerMotion.vue
@@ -2,7 +2,8 @@
 <div class="Frame840" style="width: 1440px; height: 667px; justify-content: flex-end; align-items: center; display: inline-flex">
   <div class="Group72" style="width: 1831.08px; height: 1104.07px; position: relative">
     <div class="Ellipse2" style="width: 608.46px; height: 764px; left: 0px; top: 89px; position: absolute; opacity: 0.30; background: rgba(0, 171, 190, 0.70); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
-    <div class="Ellipse3" style="width: 572px; height: 744.32px; left: 1201.53px; top: 0px; position: absolute; opacity: 0.30; background: rgba(41, 171, 226, 0.80); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
+    <div class="Ellipse6" style="width: 540.61px; height: 586.66px; left: 1290.46px; top: 74px; position: absolute; opacity: 0.30; background: rgba(0, 171, 190, 0.50); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
+    <div class="Ellipse3" style="width: 572px; height: 744.32px; left: 341.53px; top: 0px; position: absolute; opacity: 0.30; background: rgba(41, 171, 226, 0.80); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
   </div>
 </div>
 </template>
