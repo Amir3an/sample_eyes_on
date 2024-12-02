2d28ba2a8d32538bf2bd5d303700953dfe872b0d
Amir3an <amiralisayad0019@gmail.com>
2024-12-02 14:49:30 +0330

    19

--- src/assets/motion/headerMotion.vue
+++ src/assets/motion/headerMotion.vue
@@ -1,8 +1,8 @@
 <template>
-<div class="Frame840" style="width: 100%; height: 667px; align-items: center; display: inline-flex">
-  <div class="Group72" style="width: 2231.08px; height: 1104.07px; position: relative">
+<div class="Frame840" style="width: 1440px; height: 667px; justify-content: flex-end; align-items: center; display: inline-flex">
+  <div class="Group72" style="width: 1831.08px; height: 1104.07px; position: relative">
     <div class="Ellipse2" style="width: 608.46px; height: 764px; left: 0px; top: 89px; position: absolute; opacity: 0.30; background: rgba(0, 171, 190, 0.70); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
-    <div class="Ellipse6" style="width: 540.61px; height: 586.66px; left: 1690.46px; top: 37px; position: absolute; opacity: 0.30; background: rgba(0, 171, 190, 0.50); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
+    <div class="Ellipse6" style="width: 540.61px; height: 586.66px; left: 1290.46px; top: 74px; position: absolute; opacity: 0.30; background: rgba(0, 171, 190, 0.50); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
     <div class="Ellipse4" style="width: 709px; height: 780.35px; left: 604.46px; top: 37px; position: absolute; opacity: 0.30; background: rgba(0, 171, 190, 0.70); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
     <div class="Ellipse5" style="width: 597.59px; height: 750px; left: 1690.05px; top: 67px; position: absolute; transform: rotate(180deg); transform-origin: 0 0; opacity: 0.30; background: rgba(41, 171, 226, 0.90); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
     <div class="Ellipse3" style="width: 572px; height: 744.32px; left: 341.53px; top: 0px; position: absolute; opacity: 0.30; background: rgba(41, 171, 226, 0.80); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
