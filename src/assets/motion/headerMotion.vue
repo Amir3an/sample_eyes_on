1807fd66dec74c8f242a72645e9f7facf0f6576c
Amir3an <amiralisayad0019@gmail.com>
2024-12-02 16:04:08 +0330

    29

--- src/assets/motion/headerMotion.vue
+++ src/assets/motion/headerMotion.vue
@@ -3,7 +3,26 @@
   <div class="Group72" style="width: 1831.08px; height: 1104.07px; position: relative">
     <div class="Ellipse2" style="width: 608.46px; height: 764px; left: 0px; top: 89px; position: absolute; opacity: 0.30; background: rgba(0, 171, 190, 0.70); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
     <div class="Ellipse6" style="width: 540.61px; height: 586.66px; right: -25%; top: 0px; position: absolute; opacity: 0.30; background: rgba(0, 171, 190, 0.50); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
-    <div class="Ellipse3" style="width: 572px; height: 744.32px; left: 341.53px; top: 0px; position: absolute; opacity: 0.30; background: rgba(41, 171, 226, 0.80); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
+    <div class="Ellipse3" style="width: 572px; height: 444.32px; left: 30%; top: 30%; position: absolute; opacity: 0.30; background: rgba(41, 171, 226, 0.80); box-shadow: 200px 200px 200px; filter: blur(200px)"></div>
   </div>
 </div>
 </template>
+
+
+<style>
+
+.Ellipse3 {
+  animation: Ellipse3 infinite 2s alternate linear;
+}
+
+@keyframes Ellipse3 {
+  0% {
+    left : 30%;
+  }
+
+  100% {
+    left : 90%;
+  }
+}
+
+</style>
