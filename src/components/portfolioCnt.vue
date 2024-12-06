0b3708830b35d25a34b9efab616aed27bb3037e2
Amir3an <amiralisayad0019@gmail.com>
2024-12-06 14:41:15 +0330

    51

--- src/components/portfolioCnt.vue
+++ src/components/portfolioCnt.vue
@@ -1,46 +1,84 @@
 <template>
-  <swiper
-    :cssMode="true"
-    :navigation="true"
-    :pagination="true"
-    :mousewheel="true"
-    :keyboard="true"
-    :modules="modules"
-    class="mySwiper"
-  >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide><swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide><swiper-slide>Slide 5</swiper-slide>
-    <swiper-slide>Slide 6</swiper-slide><swiper-slide>Slide 7</swiper-slide>
-    <swiper-slide>Slide 8</swiper-slide><swiper-slide>Slide 9</swiper-slide>
-  </swiper>
+  <div class="portfolio-slider bg-gray-900 text-white p-8">
+    <h2 class="text-4xl font-bold mb-6">Portfolio</h2>
+    <swiper
+      :slides-per-view="1"
+      :space-between="20"
+      :breakpoints="{
+        640: { slidesPerView: 1 },
+        768: { slidesPerView: 2 },
+        1024: { slidesPerView: 3 },
+      }"
+      navigation
+      loop
+    >
+      <swiper-slide v-for="(item, index) in slides" :key="index">
+        <div class="rounded-lg shadow-lg overflow-hidden">
+          <img
+            :src="item.image"
+            alt="Design"
+            class="w-full h-48 object-cover"
+          />
+          <div class="p-4 bg-gradient-to-b from-gray-800 to-gray-900">
+            <p class="text-sm uppercase text-gray-400">Graphic Design</p>
+            <h3 class="text-lg font-semibold">{{ item.title }}</h3>
+          </div>
+        </div>
+      </swiper-slide>
+    </swiper>
+
+    <div class="swiper-navigation">
+      <button class="swiper-button-prev">
+        <i class="fas fa-arrow-left"></i>
+      </button>
+      <button class="swiper-button-next">
+        <i class="fas fa-arrow-right"></i>
+      </button>
+    </div>
+  </div>
 </template>
+
 <script>
-// Import Swiper Vue.js components
 import { Swiper, SwiperSlide } from "swiper/vue";
-
-// Import Swiper styles
 import "swiper/css";
-
 import "swiper/css/navigation";
-import "swiper/css/pagination";
-
-import "./swiper.css";
-
-// import required modules
-import { Navigation, Pagination, Mousewheel, Keyboard } from "swiper/modules";
 
 export default {
   components: {
     Swiper,
     SwiperSlide,
   },
-  setup() {
+  data() {
     return {
-      modules: [Navigation, Pagination, Mousewheel, Keyboard],
+      slides: [
+        {
+          image: "path/to/image1.jpg",
+          title: "A brand 3D Design Characteristic for growth",
+        },
+        {
+          image: "path/to/image2.jpg",
+          title: "A brand 3D Design Characteristic for growth",
+        },
+        {
+          image: "path/to/image3.jpg",
+          title: "A brand 3D Design Characteristic for growth",
+        },
+      ],
     };
   },
 };
 </script>
 
-<style scoped src="./swiper.css"></style>
+<style>
+.swiper-button-prev,
+.swiper-button-next {
+  color: #fff;
+  background: rgba(0, 0, 0, 0.5);
+  border-radius: 50%;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
