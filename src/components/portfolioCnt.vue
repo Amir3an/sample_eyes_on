c4c99017fa5d844cf56a2c662ee1234b7a162e96
Amir3an <amiralisayad0019@gmail.com>
2024-12-07 13:32:13 +0330

    53

--- src/components/portfolioCnt.vue
+++ src/components/portfolioCnt.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="w-[80%] m-auto relative portfolio-slider text-white p-8">
-    <h2 class="text-4xl font-bold mb-6">Portfolio</h2>
+    <div>
+      <h2 class="text-4xl font-bold mb-6">Portfolio</h2>
+      <div class="swiper-navigation">
+        <button class="swiper-button-prev">
+          <i class="fas fa-arrow-left"></i>
+        </button>
+        <button class="swiper-button-next">
+          <i class="fas fa-arrow-right"></i>
+        </button>
+      </div>
+    </div>
+
     <swiper
       :slides-per-view="1"
       :space-between="135"
@@ -30,15 +41,6 @@
         </div>
       </swiper-slide>
     </swiper>
-
-    <div class="swiper-navigation">
-      <button class="swiper-button-prev">
-        <i class="fas fa-arrow-left"></i>
-      </button>
-      <button class="swiper-button-next">
-        <i class="fas fa-arrow-right"></i>
-      </button>
-    </div>
   </div>
 </template>
 
