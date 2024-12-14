7ba61eed9ec7ebfe2f205b2e047b61c77bc84d54
Amir3an <amiralisayad0019@gmail.com>
2024-12-14 12:28:30 +0330

    34

--- src/components/footerCnt.vue
+++ src/components/footerCnt.vue
@@ -1,6 +1,13 @@
 <template>
   <footer class="absolute w-full h-[233px] bg-[rgba(255, 255, 255, 1)]">
-    <EyesOn class="w-[10.75rem] h-[7.8rem]" />
+    <div class="w-[80%]">
+      <EyesOn class="w-[10.75rem] h-[7.8rem]" />
+      <h2>Exelence In Every Detail</h2>
+      <p>
+        Detailed information about our motion graphics and animation services,
+        including examples and benefits.
+      </p>
+    </div>
   </footer>
 </template>
 
@@ -18,5 +25,6 @@ export default {
 footer {
   backdrop-filter: blur(20.9px);
   background: rgba(112, 249, 224, 0.08);
+  color: #fff;
 }
 </style>
