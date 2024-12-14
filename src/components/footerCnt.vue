6b96e5b7dc6504f663f22971715f79c15190b521
Amir3an <amiralisayad0019@gmail.com>
2024-12-14 10:18:23 +0330

    08

--- src/components/footerCnt.vue
+++ src/components/footerCnt.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full h-[233px] bg-[rgba(255, 255, 255, 1)]"></div>
+  <footer class="w-full h-[233px] bg-[rgba(255, 255, 255, 1)]"></footer>
 </template>
 
 <script>
@@ -7,9 +7,9 @@ export default {};
 </script>
 
 <style scpeed>
-div {
+footer {
   background: rgba(112, 249, 224, 0.08);
   backdrop-filter: blur(21px);
-  bottom: 0;
+  margin-bottom: -3rem;
 }
 </style>
