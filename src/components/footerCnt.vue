1928b19efadc806fc870e73c1188b9e99fdcb95a
Amir3an <amiralisayad0019@gmail.com>
2024-12-14 12:37:40 +0330

    39

--- src/components/footerCnt.vue
+++ src/components/footerCnt.vue
@@ -1,10 +1,10 @@
 <template>
   <footer class="absolute w-full h-[233px]">
-    <div class="flex w-[80%] h-full">
-      <div class="w-[60%] flex justify-between items-center bg-red-100">
+    <div class="flex w-[80%] h-full m-auto justify-between">
+      <div class="w-[60%] flex justify-between items-center">
         <EyesOn class="w-[10.75rem] h-[7.8rem]" />
         <article>
-          <h2 class="text-xl">Exelence In Every Detail</h2>
+          <h2 class="text-3xl">Exelence In Every Detail</h2>
           <p>
             Detailed information about our motion graphics and animation
             services, including examples and benefits.
