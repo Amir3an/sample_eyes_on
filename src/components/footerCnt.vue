86cee2f9106538cf10ad5c94719113d76c9631dc
Amir3an <amiralisayad0019@gmail.com>
2024-12-14 10:40:46 +0330

    21

--- src/components/footerCnt.vue
+++ src/components/footerCnt.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="relative w-full">
-    <footer
-      class="absolute w-full h-[233px] bg-[rgba(255, 255, 255, 1)]"
-    ></footer>
-  </div>
+  <footer
+    class="absolute w-full h-[233px] bg-[rgba(255, 255, 255, 1)]"
+  ></footer>
 </template>
 
 <style scpeed>
 footer {
   backdrop-filter: blur(20.9px);
-  background: rgb(0, 249, 203);
+  background: rgba(112, 249, 224, 0.08);
 }
 </style>
