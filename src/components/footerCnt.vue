7202c8c8a4a702334b996e11599fa1ad89b71d3c
Amir3an <amiralisayad0019@gmail.com>
2024-12-16 16:45:21 +0330

    09

--- src/components/footerCnt.vue
+++ src/components/footerCnt.vue
@@ -4,24 +4,24 @@
       <div class="flex justify-between items-center">
         <EyesOn class="w-[10.75rem] h-[7.8rem]" />
         <article class="ml-16">
-          <h2 class="text-3xl">Exelence In Every Detail</h2>
-          <p>
+          <h2 class="text-3xl font-[700]">Exelence In Every Detail</h2>
+          <p class="mt-4 font-extralight tracking-wider">
             Detailed information about our motion graphics and animation<br />
             services, including examples and benefits.
           </p>
         </article>
       </div>
 
-      <div class="flex flex-col gap-8 ml-40">
+      <div class="flex flex-col ml-[15%]">
         <h3 class="text-xl">Links</h3>
-        <div class="flex gap-8">
+        <div class="flex mt-4 gap-10">
           <button>
             <span class="text-base">Home page</span>
           </button>
           <button><span class="text-base">Services</span></button>
           <button><span class="text-base">Portfolio</span></button>
         </div>
-        <div class="flex gap-12">
+        <div class="flex mt-8 gap-12">
           <telegramIcon />
           <instagramIcon />
           <linkdenIcon />
