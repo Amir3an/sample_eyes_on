f1cf1feb759b2237c83953921cee4ec56d288df1
Amir3an <amiralisayad0019@gmail.com>
2024-12-16 17:12:50 +0330

    14

--- src/components/portfolioCnt.vue
+++ src/components/portfolioCnt.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="w-[80%] m-auto relative portfolio-slider text-white p-8">
-    <div class="flex justify-between w-full">
+    <div class="flex justify-between m-auto w-[98%]">
       <h2 class="text-4xl font-bold mb-6">Portfolio</h2>
-      <div class="swiper-navigation flex">
+      <div class="swiper-navigation flex gap-6">
         <button class="swiper-button-prev">
           <i class="fas fa-arrow-left"></i>
         </button>
@@ -12,6 +12,7 @@
       </div>
     </div>
     <swiper
+      class="mt-10"
       :slides-per-view="1"
       :space-between="135"
       :breakpoints="{
