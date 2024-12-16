87e8fc3546ca5db2e0955629db6a1f6889c1a64d
Amir3an <amiralisayad0019@gmail.com>
2024-12-16 17:06:07 +0330

    12

--- src/components/portfolioCnt.vue
+++ src/components/portfolioCnt.vue
@@ -2,7 +2,9 @@
   <div class="w-[80%] m-auto relative portfolio-slider text-white p-8">
     <div class="w-full">
       <h2 class="text-4xl font-bold mb-6">Portfolio</h2>
-      <div class="swiper-navigation absolute top-1/2 left-0 right-0 flex justify-between">
+      <div
+        class="swiper-navigation absolute top-1/2 left-0 right-0 flex justify-between"
+      >
         <button class="swiper-button-prev">
           <i class="fas fa-arrow-left"></i>
         </button>
@@ -70,6 +72,36 @@
           </div>
         </div>
       </swiper-slide>
+      <swiper-slide class="rounded-3xl">
+        <div class="relative rounded-3xl shadow-lg overflow-visible mx-5">
+          <img
+            src="../assets/img/swiper3.jfif"
+            alt="Design"
+            class="w-full h-[395px] object-cover rounded-3xl"
+          />
+          <div id="glass">
+            <p class="text-lg font-normal text-[#F1EDBA]">Graphic Design</p>
+            <h6 class="text-xl mt-2 font-bold">
+              A brand 3d Design Characteristic for growth
+            </h6>
+          </div>
+        </div>
+      </swiper-slide>
+      <swiper-slide class="rounded-3xl">
+        <div class="relative rounded-3xl shadow-lg overflow-visible mx-5">
+          <img
+            src="../assets/img/swiper1.jfif"
+            alt="Design"
+            class="w-full h-[395px] object-cover rounded-3xl"
+          />
+          <div id="glass">
+            <p class="text-lg font-normal text-[#F1EDBA]">Graphic Design</p>
+            <h6 class="text-xl mt-2 font-bold">
+              A brand 3d Design Characteristic for growth
+            </h6>
+          </div>
+        </div>
+      </swiper-slide>
     </swiper>
   </div>
 </template>
@@ -83,7 +115,6 @@ export default {
     Swiper,
     SwiperSlide,
   },
-  data() {},
 };
 </script>
 
@@ -99,11 +130,6 @@ export default {
   align-items: center;
   justify-content: center;
   z-index: 10;
-}
-
-.swiper-button-prev i,
-.swiper-button-next i {
-  font-size: 18px;
 }
 
 #glass {
