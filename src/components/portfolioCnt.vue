e0cb32c68b1a723f80ac937d45e17e79aa1f7a51
Amir3an <amiralisayad0019@gmail.com>
2024-12-16 17:15:15 +0330

    16

--- src/components/portfolioCnt.vue
+++ src/components/portfolioCnt.vue
@@ -4,10 +4,10 @@
       <h2 class="text-4xl font-bold mb-6">Portfolio</h2>
       <div class="swiper-navigation flex gap-6">
         <button class="swiper-button-prev">
-          <i class="fas fa-arrow-left"></i>
+          <arrowLeft />
         </button>
         <button class="swiper-button-next">
-          <i class="fas fa-arrow-right"></i>
+          <arrowRight />
         </button>
       </div>
     </div>
@@ -107,11 +107,15 @@
 <script>
 import { Swiper, SwiperSlide } from "swiper/vue";
 import "swiper/css";
+import arrowLeft from "../assets/svg-icon/swiper/arrow-left.vue";
+import arrowRight from "../assets/svg-icon/swiper/arrow-right.vue";
 
 export default {
   components: {
     Swiper,
     SwiperSlide,
+    arrowLeft,
+    arrowRight,
   },
 };
 </script>
